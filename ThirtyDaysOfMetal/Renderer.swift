bf8d0551563b786b41296e8d46393bd6d184046b
Kali Sina <ali.sina89@yahoo.com>
2023-05-23 16:42:51 +0200

    Create CommandQueue, CommandBuffer and CommendEncoder

--- ThirtyDaysOfMetal/Renderer.swift
+++ ThirtyDaysOfMetal/Renderer.swift
@@ -11,9 +11,50 @@ import MetalKit
 class Renderer: NSObject, MTKViewDelegate {
     
     var parent: CustomMetalView
-    
+    var metalDevice: MTLDevice!
+    var metalCommandQueue: MTLCommandQueue!
+
     init(_ parent: CustomMetalView) {
         self.parent = parent
+        guard let metalDevice = MTLCreateSystemDefaultDevice() else {
+            fatalError("metal not supported by the GPU")
+        }
+        self.metalDevice = metalDevice
+        print("Device name: ", metalDevice.name)
+
+        guard let buffer = metalDevice.makeBuffer(length: 16) else {
+            fatalError("cannot create buffer")
+        }
+
+        print("Buffer is \(buffer.length) bytes in length")
+
+        guard let metalCommandQueue = metalDevice.makeCommandQueue() else {
+            fatalError("cannot create metal commandQueue")
+        }
+
+        self.metalCommandQueue = metalCommandQueue
+
+        let commandBuffer = metalCommandQueue.makeCommandBuffer()!
+
+        guard let sourceBuffer = metalDevice.makeBuffer(length: 16) else { fatalError("sourceBuffer error") }
+        guard let destinationBuffer = metalDevice.makeBuffer(length: 16) else { fatalError("destinationBuffer error") }
+
+        let points = sourceBuffer.contents().bindMemory(to: SIMD2<Float>.self, capacity: 2)
+        points[0] = SIMD2<Float>(10,10)
+        points[1] = SIMD2<Float>(100, 100)
+
+        let blitCommandEncoder = commandBuffer.makeBlitCommandEncoder()!
+        blitCommandEncoder.copy(from: sourceBuffer, sourceOffset: 0, to: destinationBuffer, destinationOffset: 0, size: MemoryLayout<SIMD2<Float>>.stride * 2)
+
+        blitCommandEncoder.endEncoding()
+
+        commandBuffer.addCompletedHandler { completedCommandBuffer in
+            let outPoints = destinationBuffer.contents().bindMemory(to: SIMD2<Float>.self, capacity: 2)
+            let p1 = outPoints[1]
+            print("p1 in destination buffer is \(p1)")
+        }
+
+        commandBuffer.commit()
     }
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
